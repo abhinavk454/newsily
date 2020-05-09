f47b6cd9f6dd90380331e706a44e74cf0f940ad9
abhinavk454 <abhinav.vikash2@gmail.com>
2020-05-09 19:02:56 +0530

    Completed search and app v1

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDFHszUtKRZS1mBylbjH-MHfw__Okj8q4M",
-  authDomain: "newsliy.firebaseapp.com",
-  databaseURL: "https://newsliy.firebaseio.com",
-  projectId: "newsliy",
-  storageBucket: "newsliy.appspot.com",
-  messagingSenderId: "650293369216",
-  appId: "1:650293369216:web:5aed3d162f13d6032a4965",
-  measurementId: "G-5EZ8KNNKE2",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 export default firebaseConfig;
