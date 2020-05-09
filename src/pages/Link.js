fe630c0089d14c0ad9c88d6bcaedc1ce66a57f3e
abhinavk454 <abhinav.vikash2@gmail.com>
2020-05-09 18:40:26 +0530

    Added Upvote Button

--- src/pages/Link.js
+++ src/pages/Link.js
@@ -2,7 +2,14 @@ import React from "react";
 import firebase from "../firebase";
 import { Plugins } from "@capacitor/core";
 import UserContext from "../contexts/UserContext";
-import { IonPage, IonContent, IonGrid, IonRow, IonCol } from "@ionic/react";
+import {
+  IonPage,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonButton,
+} from "@ionic/react";
 import NavHeader from "../components/Headers/NavHeader";
 import { closeCircleOutline } from "ionicons/icons";
 import LinkItem from "../components/Link/linkItem";
@@ -21,11 +28,35 @@ const Link = (props) => {
   }, [linkId]);
 
   function getLink() {
+    //handles links
     linkRef.get().then((doc) => {
       setLink({ ...doc.data(), id: doc.id });
     });
   }
+
+  function handleAddVote() {
+    if (!user) {
+      props.history.push("/login");
+    } else {
+      linkRef.get().then((doc) => {
+        if (doc.exists) {
+          const previousVotes = doc.data().votes;
+          const vote = { votedBy: { id: user.uid, name: user.displayName } };
+          const updatedVotes = [...previousVotes, vote];
+          const voteCount = updatedVotes.length;
+          linkRef.update({ votes: updatedVotes, voteCount });
+          setLink((prevState) => ({
+            ...prevState,
+            votes: updatedVotes,
+            voteCount: voteCount,
+          }));
+        }
+      });
+    }
+  }
+
   function handleDeleteLink() {
+    //handles delete ing of links
     linkRef
       .delete()
       .then(() => {
@@ -62,6 +93,13 @@ const Link = (props) => {
               <IonRow>
                 <IonCol class="ion-text-center">
                   <LinkItem link={link} browser={openBrowser} />
+                  <IonButton
+                    onClick={() => handleAddVote()}
+                    size="small"
+                    shape="round"
+                  >
+                    Upvote
+                  </IonButton>
                 </IonCol>
               </IonRow>
             </IonGrid>
